30f8580cc62ee44017f230bfdb00fff3f8265276
bishalbhowmik <77568117+bishalbhowmik@users.noreply.github.com>
2023-06-08 21:17:16 +0600

    third

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fgswukf.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.DATABASE;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
